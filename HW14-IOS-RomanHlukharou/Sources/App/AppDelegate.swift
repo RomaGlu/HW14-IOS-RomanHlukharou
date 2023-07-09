540d75cd2f7f4153da14a04a828dc842d83bdb7b
RomaGlu <roma.glu.69@gmail.com>
2023-07-09 11:50:00 +0300

    project setups

---
+++ HW14-IOS-RomanHlukharou/Sources/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  HW14-IOS-RomanHlukharou
+//
+//  Created by Роман Глухарев on 09/07/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
