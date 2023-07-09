82c8120684fa010effee659bb8362368abd5c3d2
RomaGlu <roma.glu.69@gmail.com>
2023-07-09 11:42:13 +0300

    Initial Commit

---
+++ HW14-IOS-RomanHlukharou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW14-IOS-RomanHlukharou
+//
+//  Created by Роман Глухарев on 09/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
